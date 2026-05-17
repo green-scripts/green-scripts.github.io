6b78eddb22ffe929f9d9c582b5539d249dcb8962
green-scripts <greenscriptsHUB@gmail.com>
2026-05-17 16:08:26 -0300

    Monetization

---
+++ Ads/Popunder.js
@@ -0,0 +1,7 @@
+const isLocalhost = ['localhost', '127.0.0.1', '::1'].includes(window.location.hostname);
+
+if (!isLocalhost) {
+    const script = document.createElement('script');
+    script.src = 'https://pl29478718.effectivecpmnetwork.com/4c/d7/c9/4cd7c936b31b7dd8b743a86d366cd719.js';
+    document.head.appendChild(script);
+}
---
+++ Ads/SocialBar.js
@@ -0,0 +1,7 @@
+const isLocalhost = ['localhost', '127.0.0.1', '::1'].includes(window.location.hostname);
+
+if (!isLocalhost) {
+    const script = document.createElement('script');
+    script.src = '"https://pl29328237.effectivecpmnetwork.com/69/48/71/694871912c33e6042fa1c2eb0b8d6988.js';
+    document.head.appendChild(script);
+}
