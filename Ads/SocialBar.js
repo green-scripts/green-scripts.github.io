48029d81cd387764b1aa9b79af96e48764d3abc8
green-scripts <greenscriptsHUB@gmail.com>
2026-05-20 14:54:35 -0300

    FIX Syntax Error: Ads/SocialBar

--- Ads/SocialBar.js
+++ Ads/SocialBar.js
@@ -2,6 +2,6 @@ const isLocalhost = ['localhost', '127.0.0.1', '::1'].includes(window.location.h
 
 if (!isLocalhost) {
     const script = document.createElement('script');
-    script.src = '"https://pl29328237.effectivecpmnetwork.com/69/48/71/694871912c33e6042fa1c2eb0b8d6988.js';
+    script.src = 'https://pl29328237.effectivecpmnetwork.com/69/48/71/694871912c33e6042fa1c2eb0b8d6988.js';
     document.head.appendChild(script);
 }
